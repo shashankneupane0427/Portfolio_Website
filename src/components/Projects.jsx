e137a0fb07eabc126349e555a0d8dca2015f13cb
Shashank Neupane <suryaneupane0427@gmail.com>
2025-01-12 19:24:08 +0545

    `Removed unused image assets and updated Projects component to use new image imports`

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,44 @@
 import React from 'react'
-import Portfolio from '../assets/portfolio.png'
-import SpicyBites from '../assets/SpicyBites.png'
-import Youtube from '../assets/Youtube.png'
-import Webelite from '../assets/Webelite.png'
-import Supercar from '../assets/Supercar.png'
 import Cards from './Cards'
+import foodie from '../assets/foodie.png'
+import realestate from '../assets/realstate.png'
 
 
 const Projects = () => {
 
   const projectJson = [
     {
-      title: 'Portfolio',
-      desc: 'Webelite Builders did an amazing job on our website. Their professionalism and dedication to our project were outstanding.',
-      image: Portfolio,
+      title: 'Ghar Bazar',
+      desc: 'A fullstack Real Estate website built with React, Node.js, Express and MongoDB.',
+      image: realestate,
       live: "",
       github: ""
     },
     {
-      title: 'Spicy Bites',
-      desc: 'The team at Webelite Builders exceeded our expectations with their digital marketing expertise. Highly recommend!',
-      image: Portfolio,
+      title: 'Foodie',
+      desc: 'A Food Odering website landing page built with React and Tailwind CSS.',
+      image: foodie,
       live: "",
       github: ""
     },
     {
       title: 'YouTube Clone',
       desc: 'Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.',
-      image: Portfolio,
+      image: '',
       live: "",
       github: ""
     },
     {
       title: 'Webelite builder',
       desc: 'Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.',
-      image: Portfolio,
+      image: '',
       live: "",
       github: ""
     },
     {
       title: 'Super Car',
       desc: 'Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.',
-      image: Portfolio,
+      image: '',
       live: "",
       github: ""
     },
